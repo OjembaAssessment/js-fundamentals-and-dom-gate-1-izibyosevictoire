139f6b9bffad1e896a397ed3436ecfe4cb5f4970
victoire <luandavicky5@gmail.com>
2023-07-24 10:54:23 +0200

    deploy

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,46 @@
-console.log("Exercise 3 - Timer");
+
+function add() {
+    let A = document.getElementById("timer").innerHTML
+    console.log(A)
+    document.getElementById("timer").innerHTML = parseInt(A) + 1;
+}
+function minus() {
+    let B = document.getElementById("timer").innerHTML
+    document.getElementById("timer").innerHTML = parseInt(B) - 1
+}
+function reset() {
+    document.getElementById("timer").innerHTML = 0
+}
+function countDown() {
+    const count = document.querySelector('#timer')
+    let start = document.querySelector('#start');
+    let stop = document.querySelector('#stop');
+    start.addEventListener('click', function() {
+        let timerId = setInterval(function() {
+            console.log('!')
+        }, 1000);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const interval = setInterval(() => {
+    //     if (count.textContent === '0') {
+    //         clearInterval(interval)
+    //         document.querySelector('.start').classList.add('hidden')
+    //     } else {
+    //         count.textContent = parseInt(count.textContent) - 1
+    //     }
+    // }, 1000)
+    // return interval
+}
